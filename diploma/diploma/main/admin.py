0481a0bcd9114c029054d35e7f064714f9310732
VitaliyaKulinenkp <v_bezhok@mail.ru>
2023-01-26 00:50:13 +0300

    add registration

--- diploma/diploma/main/admin.py
+++ diploma/diploma/main/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Timetable, Blog
+from .models import Timetable, Blog, Registration
+
 admin.site.register(Timetable)
 admin.site.register(Blog)
+admin.site.register(Registration)
